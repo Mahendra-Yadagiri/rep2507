4c7f79b15354c5ac0da0dd0ccc8831fdf118024b
Mahendra-Yadagiri <mahendrayadagiri45@gmail.com>
2025-10-22 19:36:56 +0530

    modified App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
      <h1>This code is written by Mahendra on behalf of Himaja</h1>
+     <h1>This code is written by Mahendra</h1>
     </div>
   );
 }
