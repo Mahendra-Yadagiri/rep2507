b1e34a1ed5f246d7cd6e6f622195682d594a7cfd
Himaja <himajadayyala@gmail.com>
2025-10-26 10:49:16 +0530

    edited with heading and paragraph

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
      <h1>This code is written by Mahendra on behalf of Himaja</h1>
      <h1>This code is written by Mahendra</h1>
+     <h2>This code is edited by Himaja</h2>
+     <p>We are practicing Git and GitHub collaboration. Concepts we are learning are local repositry, remote repositry, clone , push and pull.</p>
     </div>
   );
 }
